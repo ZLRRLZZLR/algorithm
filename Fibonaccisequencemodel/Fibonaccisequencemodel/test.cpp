0e5789c3b8db7026d7f9c5dfda5de1c6f9e4b9b8
zhou-liurong <19551230598@163.com>
2024-11-15 19:31:42 +0800

    Decoding Method of Dynamic Programming Algorithm (Medium)

--- Fibonaccisequencemodel/Fibonaccisequencemodel/test.cpp
+++ Fibonaccisequencemodel/Fibonaccisequencemodel/test.cpp
@@ -1,5 +1,64 @@
 
-
+//解码方法（medium）
+//class Solution {
+//public:
+//    int numDecodings(string s) {
+//
+//        int n = s.size();
+//        vector<int> dp(n + 1);
+//
+//        dp[0] = 1;
+//        dp[1] = s[0] != '0';
+//
+//        for (int i = 2; i <= n; i++)
+//        {
+//            if (s[i - 1] >= '1' && s[i - 1] <= '9')
+//                dp[i] += dp[i - 1];
+//
+//            int num = (s[i - 2] - '0') * 10 + (s[i - 1] - '0');
+//            if (num >= 10 && num <= 26)
+//                dp[i] += dp[i - 2];
+//        }
+//
+//        return dp[n];
+//
+//    }
+//};
+//class Solution {
+//public:
+//    int numDecodings(string s) {
+//        if (s[0] == '0')
+//            return 0;
+//
+//        int n = s.size();
+//        vector<int> dp(n);
+//
+//        dp[0] = 1;
+//
+//        if (n >= 2)
+//        {
+//            if (s[1] >= '1' && s[1] <= '9')
+//                dp[1] += 1;
+//
+//            int num = (s[0] - '0') * 10 + (s[1] - '0');
+//            if (num >= 10 && num <= 26)
+//                dp[1] += 1;
+//        }
+//
+//        for (int i = 2; i < n; i++)
+//        {
+//            if (s[i] >= '1' && s[i] <= '9')
+//                dp[i] += dp[i - 1];
+//
+//            int num = (s[i - 1] - '0') * 10 + (s[i] - '0');
+//            if (num >= 10 && num <= 26)
+//                dp[i] += dp[i - 2];
+//        }
+//
+//        return dp[n - 1];
+//
+//    }
+//};
 //使用最小花费爬楼梯（easy）解法一
 //class Solution {
 //public:
