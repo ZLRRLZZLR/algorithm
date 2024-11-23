b4f8ecd6dece8c769aaa760acde9c5f77bc30049
zhou-liurong <19551230598@163.com>
2024-11-23 22:39:24 +0800

    daily worK

--- Routing_issues/Routing_issues/Routing_issues.cpp
+++ Routing_issues/Routing_issues/Routing_issues.cpp
@@ -1,4 +1,25 @@
 
+//地下城游戏（hard）
+//class Solution {
+//public:
+//    int calculateMinimumHP(vector<vector<int>>& dungeon) {
+//        int m = dungeon.size();
+//        int n = dungeon[0].size();
+//        vector<vector<int>> dp(m + 1, vector<int>(n + 1, INT_MAX));
+//        dp[m - 1][n] = 1;
+//        dp[m][n - 1] = 1;
+//        for (int i = m - 1; i >= 0; i--)
+//        {
+//            for (int j = n - 1; j >= 0; j--)
+//            {
+//                dp[i][j] = min(dp[i + 1][j], dp[i][j + 1]) - dungeon[i][j];
+//                dp[i][j] = max(1, dp[i][j]);
+//            }
+//        }
+//
+//        return dp[0][0];
+//    }
+//};
 
 //最小路径和
 /*class Solution {
