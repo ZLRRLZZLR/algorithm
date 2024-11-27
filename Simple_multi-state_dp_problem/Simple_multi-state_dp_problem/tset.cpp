8e846ea5b1bf96cd32ce186fc1cf023d3b94b94c
zhou-liurong <19551230598@163.com>
2024-11-28 00:40:23 +0800

    打家劫舍II （medium）

--- Simple_multi-state_dp_problem/Simple_multi-state_dp_problem/tset.cpp
+++ Simple_multi-state_dp_problem/Simple_multi-state_dp_problem/tset.cpp
@@ -1,4 +1,33 @@
 
+
+//打家劫舍II （medium）
+//class Solution {
+//public:
+//    int rob(vector<int>& nums) {
+//
+//        int n = nums.size();
+//
+//        return max(nums[0] + robel(nums, 2, n - 2), robel(nums, 1, n - 1));
+//    }
+//    int robel(vector<int>& nums, int left, int right)
+//    {
+//        if (left > right) return 0;
+//        int n = nums.size();
+//        vector<int> f(n);
+//        auto g = f;
+//
+//        f[left] = nums[left];
+//
+//        for (int i = left + 1; i <= right; i++)
+//        {
+//            f[i] = g[i - 1] + nums[i];
+//            g[i] = max(f[i - 1], g[i - 1]);
+//        }
+//
+//        return max(f[right], g[right]);
+//    }
+//};
+
 //按摩师（easy）
 //class Solution {
 //public:
