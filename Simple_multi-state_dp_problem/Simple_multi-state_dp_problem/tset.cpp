1e99a6c084fc54d5d5ac48d8fe928a5517f0b50f
zhou-liurong <19551230598@163.com>
2024-12-05 00:02:14 +0800

    粉刷房子（medium）

--- Simple_multi-state_dp_problem/Simple_multi-state_dp_problem/tset.cpp
+++ Simple_multi-state_dp_problem/Simple_multi-state_dp_problem/tset.cpp
@@ -1,4 +1,21 @@
 
+
+//粉刷房子（medium）
+//class Solution {
+//public:
+//    int minCost(vector<vector<int>>& costs) {
+//        int n = costs.size();
+//        vector<vector<int>> dp(n + 1, vector<int>(3));
+//        for (int i = 1; i <= n; i++)
+//        {
+//            dp[i][0] = min(dp[i - 1][1], dp[i - 1][2]) + costs[i - 1][0];
+//            dp[i][1] = min(dp[i - 1][0], dp[i - 1][2]) + costs[i - 1][1];
+//            dp[i][2] = min(dp[i - 1][1], dp[i - 1][0]) + costs[i - 1][2];
+//        }
+//
+//        return (min(dp[n][1], min(dp[n][0], dp[n][2])));
+//    }
+//};
 //删除并获得点数（medium）
 //class Solution {
 //public:
