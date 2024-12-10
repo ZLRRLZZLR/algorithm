1838f90290a1608c8bfa33c9151cbfd941831c1f
zhou-liurong <19551230598@163.com>
2024-12-11 00:03:19 +0800

    最大子数组和（medium）

--- Binary_search/Binary_search/源.cpp
+++ Binary_search/Binary_search/源.cpp
@@ -1,19 +1,54 @@
-class Solution {
-public:
-    int maxSubArray(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> dp(n + 1);
-        for (int i = 1; i <= n; i++)
-        {
-            dp[i] = max(nums[i - 1], dp[i - 1] + nums[i - 1]);
-        }
+#include<iostream>
+#include<vector>
+using namespace std;
 
-        int ret = INT_MIN;
-
-        for (auto& e : nums)
-        {
-            ret = max(ret, e);
-        }
-        return ret;
-    }
-};
+//最大子数组和（medium）
+//class Solution {
+//public:
+//    int maxSubArray(vector<int>& nums) {
+//        int n = nums.size();
+//
+//        vector<int> dp(n + 1);
+//
+//        for (int i = 1; i <= n; i++)
+//        {
+//            dp[i] = max(nums[i - 1], dp[i - 1] + nums[i - 1]);
+//        }
+//
+//        int ret = INT_MIN;
+//
+//        for (int i = 1; i <= n; i++)
+//        {
+//            ret = max(ret, dp[i]);
+//        }
+//
+//        return ret;
+//    }
+//};
+//class Solution {
+//public:
+//    int maxSubArray(vector<int>& nums) {
+//        int n = nums.size();
+//        vector<int> dp(n + 1);
+//        for (int i = 1; i <= n; i++)
+//        {
+//            dp[i] = max(nums[i - 1], dp[i - 1] + nums[i - 1]);
+//        }
+//
+//        int ret = INT_MIN;
+//
+//        for (auto& e : dp)
+//        {
+//            ret = max(ret, e);
+//        }
+//
+//        return ret;
+//    }
+//};
+//
+//int main()
+//{
+//    vector<int> v = { -1 };
+//    Solution().maxSubArray(v);
+//    return 0;
+//}
