bc7ee668480537028e87e4e7d2f2afdacaea5b45
zhou-liurong <19551230598@163.com>
2024-12-14 20:47:35 +0800

    在排序数组中查找元素的第一个和最后一个位置（medium）

--- Binary_search/Binary_search/源.cpp
+++ Binary_search/Binary_search/源.cpp
@@ -3,6 +3,55 @@
 using namespace std;
 
 
+//class Solution {
+//public:
+//    vector<int> searchRange(vector<int>& nums, int target) {
+//        int n = nums.size();
+//        vector<int> ret(2, -1);
+//
+//        if (n == 0) return ret;
+//
+//        int left = 0, right = n - 1;
+//        while (left < right)
+//        {
+//            int mid = left + ((right - left) >> 1);
+//            if (nums[mid] < target)
+//            {
+//                left = mid + 1;
+//            }
+//            else
+//            {
+//                right = mid;
+//            }
+//        }
+//        if (nums[left] == target) ret[0] = left;
+//        else { ret[0] = -1; ret[1] = -1; return ret; }
+//
+//        for (right = n - 1; left < right;)
+//        {
+//            int mid = left + ((right - left + 1) >> 1);
+//            if (nums[mid] <= target)
+//            {
+//                left = mid;
+//            }
+//            else
+//            {
+//                right = mid - 1;
+//            }
+//        }
+//        if (nums[left] == target) ret[1] = left;
+//
+//        return ret;
+//
+//    }
+//};
+//
+//int main()
+//{
+//    vector<int> v = {1};
+//    Solution().searchRange(v,1);
+//    return 0;
+//}
 //¶þ·Ö²éÕÒ£¨easy£©
 //class Solution {
 //public:
