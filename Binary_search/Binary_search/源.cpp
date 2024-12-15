4e9fefbb38d652545c63c71bb7a65dfeb1a65e2e
zhou-liurong <19551230598@163.com>
2024-12-16 00:16:18 +0800

    寻找峰值（medium）

--- Binary_search/Binary_search/源.cpp
+++ Binary_search/Binary_search/源.cpp
@@ -3,6 +3,22 @@
 using namespace std;
 
 
+//—∞’“∑Â÷µ£®medium£©
+//class Solution {
+//public:
+//    int findPeakElement(vector<int>& nums) {
+//        int left = 0, right = nums.size() - 1;
+//        if (nums.size() == 1) return right;
+//
+//        while (left < right) {
+//            int mid = left + ((right - left + 1) >> 1);
+//            if (nums[mid] > nums[mid - 1]) left = mid;
+//            else right = mid - 1;
+//        }
+//
+//        return left;
+//    }
+//};
 //À—À˜≤Â»ÎŒª÷√£®easy£©
 //class Solution {
 //public:
