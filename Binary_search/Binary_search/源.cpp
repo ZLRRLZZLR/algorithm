053f214e00cad71a11a20f175be1b052e4f1aab5
zhou-liurong <19551230598@163.com>
2024-12-16 23:34:47 +0800

    搜索旋转排序数组中的最小值（medium）和852. 山脉数组的峰顶索引

--- Binary_search/Binary_search/源.cpp
+++ Binary_search/Binary_search/源.cpp
@@ -3,6 +3,50 @@
 using namespace std;
 
 
+
+//搜索旋转排序数组中的最小值（medium）
+//class Solution {
+//public:
+//    int findMin(vector<int>& nums) {
+//        int n = nums.size();
+//        int left = 0, right = n - 1;
+//        while (left < right) {
+//            int mid = left + ((right - left) >> 1);
+//            if (nums[mid] >= nums[0]) left = mid + 1;
+//            else right = mid;
+//        }
+//        return nums[left] > nums[0] ? nums[0] : nums[left];
+//    }
+//};
+////class Solution {
+////public:
+////    int findMin(vector<int>& nums) {
+////        int n = nums.size();
+////        int left = 0, right = n - 1;
+////        while (left < right) {
+////            int mid = left + ((right - left) >> 1);
+////            if (nums[mid] > nums[n - 1]) left = mid + 1;
+////            else right = mid;
+////        }
+////        return nums[left];
+////    }
+////};
+//852. 山脉数组的峰顶索引
+//class Solution {
+//public:
+//    int peakIndexInMountainArray(vector<int>& arr) {
+//        int left = 0, right = arr.size() - 1;
+//        if (arr.size() == 1) return right;
+//
+//        while (left < right) {
+//            int mid = left + ((right - left + 1) >> 1);
+//            if (arr[mid] > arr[mid - 1]) left = mid;
+//            else right = mid - 1;
+//        }
+//
+//        return left;
+//    }
+//};
 //寻找峰值（medium）
 //class Solution {
 //public:
