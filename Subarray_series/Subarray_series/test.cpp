c7675d1fad27be2de0c39f439e43f3b5612dd034
zhou-liurong <19551230598@163.com>
2024-12-18 22:59:38 +0800

    乘积为正数的最长子数组（medium）

--- Subarray_series/Subarray_series/test.cpp
+++ Subarray_series/Subarray_series/test.cpp
@@ -3,6 +3,29 @@
 
 using namespace std;
 
+//乘积为正数的最长子数组（medium）
+//class Solution {
+//public:
+//    int getMaxLen(vector<int>& nums) {
+//        int n = nums.size();
+//        vector<int> f(n + 1);
+//        auto g = f;
+//        int ret = INT_MIN;
+//        for (int i = 1; i <= n; i++) {
+//            if (nums[i - 1] > 0) {
+//                f[i] = f[i - 1] + 1;
+//                g[i] = g[i - 1] == 0 ? 0 : g[i - 1] + 1;
+//            }
+//            else if (nums[i - 1] < 0) {
+//                f[i] = g[i - 1] == 0 ? 0 : g[i - 1] + 1;
+//                g[i] = f[i - 1] + 1;
+//            }
+//            ret = max(ret, f[i]);
+//        }
+//
+//        return ret;
+//    }
+//};
 //乘积最大子数组（medium）
 //class Solution {
 //public:
