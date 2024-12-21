f048ba83cac3a4c144591e5dfc2b11ca73648612
zhou-liurong <19551230598@163.com>
2024-12-21 23:22:20 +0800

    寻找数组的中心下标（easy）

--- Prefix_Sum/Prefix_Sum/test.cpp
+++ Prefix_Sum/Prefix_Sum/test.cpp
@@ -1,4 +1,26 @@
 
+//
+//寻找数组的中心下标（easy）
+//class Solution {
+//public:
+//    int pivotIndex(vector<int>& nums) {
+//        int n = nums.size();
+//        vector<int> f(n), g(n);
+//        for (int i = 1; i < n; i++) {
+//            f[i] = f[i - 1] + nums[i - 1];
+//        }
+//        for (int i = n - 2; i >= 0; i--) {
+//            g[i] = g[i + 1] + nums[i + 1];
+//        }
+//
+//        for (int i = 0; i < n; i++) {
+//            if (f[i] == g[i])
+//                return i;
+//        }
+//        return -1;
+//    }
+//};
+//【模板】二维前缀和（medium）
 //#include <iostream>
 //#include <vector>
 //using namespace std;
