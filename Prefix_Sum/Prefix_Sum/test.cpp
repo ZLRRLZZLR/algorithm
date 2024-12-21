0b24f83f173e7f1a793090d9ad303abc463226b8
zhou-liurong <19551230598@163.com>
2024-12-21 21:45:22 +0800

    【模板】二维前缀和（medium）

--- Prefix_Sum/Prefix_Sum/test.cpp
+++ Prefix_Sum/Prefix_Sum/test.cpp
@@ -1,4 +1,32 @@
 
+//#include <iostream>
+//#include <vector>
+//using namespace std;
+//
+//int main() {
+//    int n = 0, m = 0, q = 0;
+//    cin >> n >> m >> q;
+//    vector<vector<int>> nums(n + 1, vector<int>(m + 1));
+//    for (int i = 1; i <= n; i++) {
+//        for (int j = 1; j <= m; j++) {
+//            cin >> nums[i][j];
+//        }
+//    }
+//    vector<vector<long long>> dp(n + 1, vector<long long>(m + 1));
+//    for (int i = 1; i <= n; i++) {
+//        for (int j = 1; j <= m; j++) {
+//            dp[i][j] = dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - 1] + nums[i][j];
+//        }
+//    }
+//    int x1, x2, y1, y2;
+//    while (q--) {
+//        cin >> x1 >> y1 >> x2 >> y2;
+//        cout << dp[x2][y2] - dp[x2][y1 - 1] - dp[x1 - 1][y2] + dp[x1 - 1][y1 - 1] << endl;
+//    }
+//}
+// 64 位输出请用 printf("%lld")
+// 64 位输出请用 printf("%lld")
+
 //【模板】前缀和
 //#include <iostream>
 //#include<vector>
