ad5b8a695b676d7b06ced94680b628d836c03353
zhou-liurong <19551230598@163.com>
2024-12-22 17:57:48 +0800

    除自身以外数组的乘积（medium）

--- Prefix_Sum/Prefix_Sum/test.cpp
+++ Prefix_Sum/Prefix_Sum/test.cpp
@@ -1,4 +1,22 @@
 
+//除自身以外数组的乘积（medium）
+//class Solution {
+//public:
+//    vector<int> productExceptSelf(vector<int>& nums) {
+//        int n = nums.size();
+//        vector<int> f(n, 1), g(n, 1), ret(n);
+//        for (int i = 1; i < n; i++) {
+//            f[i] = f[i - 1] * nums[i - 1];
+//        }
+//        for (int i = n - 2; i >= 0; i--) {
+//            g[i] = g[i + 1] * nums[i + 1];
+//        }
+//        for (int i = 0; i < n; i++)
+//            ret[i] = f[i] * g[i];
+//
+//        return ret;
+//    }
+//};
 //
 //寻找数组的中心下标（easy）
 //class Solution {
