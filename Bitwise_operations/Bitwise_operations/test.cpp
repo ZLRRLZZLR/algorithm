6286b1d7670f957d88089f96da6134f8c6423169
zhou-liurong <19551230598@163.com>
2024-12-27 18:52:33 +0800

    比特位计数

--- Bitwise_operations/Bitwise_operations/test.cpp
+++ Bitwise_operations/Bitwise_operations/test.cpp
@@ -1,4 +1,21 @@
 
+//比特位计数
+//class Solution {
+//public:
+//    vector<int> countBits(int n) {
+//        vector<int> ret(n + 1);
+//        for (int i = 0; i <= n; i++) {
+//            int count = 0, tmp = i;
+//            while (tmp) {
+//                count++;
+//                tmp = tmp & (tmp - 1);
+//            }
+//            ret[i] = count;
+//        }
+//
+//        return ret;
+//    }
+//};
 //位1的个数
 //class Solution {
 //public:
