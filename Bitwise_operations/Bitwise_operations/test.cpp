889c41a13fc664d8eecd8602a2b20d7a68e846f3
zhou-liurong <19551230598@163.com>
2024-12-27 18:51:44 +0800

    位1的个数

---
+++ Bitwise_operations/Bitwise_operations/test.cpp
@@ -0,0 +1,13 @@
+
+//位1的个数
+//class Solution {
+//public:
+//    int hammingWeight(int n) {
+//        int ret = 0;
+//        while (n) {
+//            ret++;
+//            n = n & (n - 1);
+//        }
+//        return ret;
+//    }
+//};
