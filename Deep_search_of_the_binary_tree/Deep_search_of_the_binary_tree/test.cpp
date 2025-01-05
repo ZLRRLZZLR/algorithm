9357947a7ba843b2d44a9d0346ca0ce01fc50dcb
zhou-liurong <19551230598@163.com>
2025-01-05 20:57:46 +0800

    求根节点到叶节点数字之和（medium）

--- Deep_search_of_the_binary_tree/Deep_search_of_the_binary_tree/test.cpp
+++ Deep_search_of_the_binary_tree/Deep_search_of_the_binary_tree/test.cpp
@@ -1,5 +1,31 @@
 
 
+///**求根节点到叶节点数字之和（medium）
+// * Definition for a binary tree node.
+// * struct TreeNode {
+// *     int val;
+// *     TreeNode *left;
+// *     TreeNode *right;
+// *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+// *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+// *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+// * };
+// */
+//class Solution {
+//public:
+//    int sumNumbers(TreeNode* root) {
+//        return _sumNumbers(root, 0);
+//    }
+//    int _sumNumbers(TreeNode* root, int presum) {
+//        presum = presum * 10 + root->val;
+//        if (root->left == nullptr && root->right == nullptr)
+//            return presum;
+//        int ret = 0;
+//        if (root->left) ret += _sumNumbers(root->left, presum);
+//        if (root->right) ret += _sumNumbers(root->right, presum);
+//        return ret;
+//    }
+//};
 ///**计算布尔二叉树的值（medium）
 // * Definition for a binary tree node.
 // * struct TreeNode {
