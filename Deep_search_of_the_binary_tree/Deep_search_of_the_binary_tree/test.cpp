5eacc7bc93956a74f98616be1c5e3366fb579e20
zhou-liurong <19551230598@163.com>
2025-01-05 20:33:10 +0800

    计算布尔二叉树的值（medium）

---
+++ Deep_search_of_the_binary_tree/Deep_search_of_the_binary_tree/test.cpp
@@ -0,0 +1,22 @@
+
+
+///**计算布尔二叉树的值（medium）
+// * Definition for a binary tree node.
+// * struct TreeNode {
+// *     int val;
+// *     TreeNode *left;
+// *     TreeNode *right;
+// *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+// *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+// *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+// * };
+// */
+//class Solution {
+//public:
+//    bool evaluateTree(TreeNode* root) {
+//        if (root->left == nullptr) return root->val;
+//        bool left = evaluateTree(root->left);
+//        bool right = evaluateTree(root->right);
+//        return root->val == 2 ? left || right : left && right;
+//    }
+//};
