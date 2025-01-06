fe5fcca866222987166040f54e8f1d9843bd86ec
zhou-liurong <19551230598@163.com>
2025-01-06 18:57:19 +0800

    二叉搜索树中第 k 小的元素（medium）

--- Deep_search_of_the_binary_tree/Deep_search_of_the_binary_tree/test.cpp
+++ Deep_search_of_the_binary_tree/Deep_search_of_the_binary_tree/test.cpp
@@ -1,4 +1,32 @@
 
+///**二叉搜索树中第 k 小的元素（medium）
+// * Definition for a binary tree node.
+// * struct TreeNode {
+// *     int val;
+// *     TreeNode *left;
+// *     TreeNode *right;
+// *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+// *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+// *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+// * };
+// */
+//class Solution {
+//    int count;
+//    int ret;
+//public:
+//    int kthSmallest(TreeNode* root, int k) {
+//        count = k;
+//        dfs(root);
+//        return ret;
+//    }
+//    void dfs(TreeNode* root) {
+//        if (root == nullptr || count == 0) return;
+//        dfs(root->left);
+//        count--;
+//        if (count == 0) ret = root->val;
+//        dfs(root->right);
+//    }
+//};
 ///**验证二叉搜索树（medium）
 // * Definition for a binary tree node.
 // * struct TreeNode {
