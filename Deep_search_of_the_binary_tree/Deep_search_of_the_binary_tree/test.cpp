edb7405972daab582213f8c016e9a3f0bdaf2d44
zhou-liurong <19551230598@163.com>
2025-01-06 09:37:48 +0800

    二叉树剪枝（medium）

--- Deep_search_of_the_binary_tree/Deep_search_of_the_binary_tree/test.cpp
+++ Deep_search_of_the_binary_tree/Deep_search_of_the_binary_tree/test.cpp
@@ -1,5 +1,29 @@
 
-
+///**二叉树剪枝（medium）
+// * Definition for a binary tree node.
+// * struct TreeNode {
+// *     int val;
+// *     TreeNode *left;
+// *     TreeNode *right;
+// *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+// *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+// *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+// * };
+// */
+//class Solution {
+//public:
+//    TreeNode* pruneTree(TreeNode* root) {
+//        if (root == nullptr)
+//            return nullptr;
+//        root->left = pruneTree(root->left);
+//        root->right = pruneTree(root->right);
+//        if (root->left == nullptr && root->right == nullptr && root->val == 0)
+//        {
+//            root = nullptr;
+//        }
+//        return root;
+//    }
+//};
 ///**求根节点到叶节点数字之和（medium）
 // * Definition for a binary tree node.
 // * struct TreeNode {
