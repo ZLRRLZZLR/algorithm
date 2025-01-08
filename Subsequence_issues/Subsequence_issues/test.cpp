b9316e374ee2dfe2ceaa399f5c82e43982340a73
zhou-liurong <19551230598@163.com>
2025-01-08 22:10:13 +0800

    最长的斐波那契子序列的长度（medium）

--- Subsequence_issues/Subsequence_issues/test.cpp
+++ Subsequence_issues/Subsequence_issues/test.cpp
@@ -4,6 +4,27 @@
 #include<algorithm>
 using namespace std;
 
+//最长的斐波那契子序列的长度（medium）
+//class Solution {
+//public:
+//    int lenLongestFibSubseq(vector<int>& arr) {
+//        int n = arr.size();
+//        vector<vector<int>> dp(n, vector<int>(n, 2));
+//        unordered_map<int, int> hash;
+//        for (int i = 0; i < n; i++) hash[arr[i]] = i;
+//
+//        int ret = 2;
+//        for (int j = 2; j < n; j++) {
+//            for (int i = 1; i < j; i++) {
+//                int a = arr[j] - arr[i];
+//                if (a < arr[i] && hash.count(a))
+//                    dp[i][j] = dp[hash[a]][i] + 1;
+//                ret = max(ret, dp[i][j]);
+//            }
+//        }
+//        return ret < 3 ? 0 : ret;
+//    }
+//};
 //最长定差子序列（medium）
 /*class Solution {
 public:
