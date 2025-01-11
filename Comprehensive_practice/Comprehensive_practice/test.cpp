186f8f68973751f0da7995d53fc7c1ebca81dd18
zhou-liurong <19551230598@163.com>
2025-01-11 23:11:20 +0800

    全排列 Ⅱ（medium）

--- Comprehensive_practice/Comprehensive_practice/test.cpp
+++ Comprehensive_practice/Comprehensive_practice/test.cpp
@@ -1,3 +1,65 @@
+
+
+//全排列 Ⅱ（medium）
+//class Solution {
+//public:
+//	bool check[9];
+//	vector<int> path;
+//	vector<vector<int>> ret;
+//	vector<vector<int>> permuteUnique(vector<int>& nums) {
+//		sort(nums.begin(), nums.end());
+//		dfs(nums);
+//		return ret;
+//	}
+//	void dfs(vector<int>& nums) {
+//		if (path.size() == nums.size()) {
+//			ret.push_back(path);
+//		}
+//		for (int i = 0; i < nums.size(); i++) {
+//			if (!(check[i] == true || (i != 0 && nums[i] == nums[i - 1] && check[i - 1] == false))) {
+//				path.push_back(nums[i]);
+//				check[i] = true;
+//				dfs(nums);
+//				path.pop_back();
+//				check[i] = false;
+//			}
+//		}
+//	}
+//};
+//class Solution
+//{
+//	vector<int> path;
+//	vector<vector<int>> ret;
+//	bool check[9];
+//public:
+//	vector<vector<int>> permuteUnique(vector<int>& nums)
+//	{
+//		sort(nums.begin(), nums.end());
+//		dfs(nums, 0);
+//		return ret;
+//	}
+//	void dfs(vector<int>& nums, int pos)
+//	{
+//		if (pos == nums.size())
+//		{
+//			ret.push_back(path);
+//			return;
+//		}
+//		for (int i = 0; i < nums.size(); i++)
+//		{
+//			// 剪枝
+//			if (check[i] == false && (i == 0 || nums[i] != nums[i - 1] ||
+//				check[i - 1] != false))
+//			{
+//				path.push_back(nums[i]);
+//				check[i] = true;
+//				dfs(nums, pos + 1);
+//				path.pop_back(); // 恢复现场
+//				check[i] = false;
+//			}
+//		}
+//	}
+//};
 //找出所有子集的异或总和再求和（easy）
 //class Solution {
 //    int sum;
