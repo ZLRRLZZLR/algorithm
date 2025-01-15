ff2d964f0a620720762f3dab9daa2737e74f865a
zhou-liurong <19551230598@163.com>
2025-01-15 21:42:38 +0800

    分割回文串II（hard）

--- 回文串问题/回文串问题/test.cpp
+++ 回文串问题/回文串问题/test.cpp
@@ -4,6 +4,35 @@
 using namespace std;
 
 
+//分割回文串II（hard）
+//class Solution {
+//public:
+//    int minCut(string s) {
+//        int n = s.size();
+//        vector<vector<bool>> dp(n, vector<bool>(n));
+//        for (int i = n - 1; i >= 0; i--) {
+//            for (int j = i; j < n; j++) {
+//                if (s[i] == s[j]) i + 1 >= j ? dp[i][j] = true : dp[i][j] = dp[i + 1][j - 1];
+//            }
+//        }
+//
+//        vector<int> ret(n, INT_MAX);
+//        for (int i = 0; i < n; i++) {
+//            if (dp[0][i]) ret[i] = 0;
+//            else {
+//                for (int j = 1; j <= i; j++) {
+//                    if (dp[j][i]) ret[i] = min(ret[j - 1] + 1, ret[i]);
+//                }
+//            }
+//        }
+//
+//        return ret[n - 1];
+//    }
+//};
+//int main() {
+//
+//    return 0;
+//}
 //回文串分割IV（hard）
 //class Solution {
 //public:
