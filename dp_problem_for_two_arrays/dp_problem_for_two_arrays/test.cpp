59be190471b6642857745a3dd110b0631ff0eb00
zhou-liurong <19551230598@163.com>
2025-01-19 23:50:59 +0800

    最长公共子序列（medium）

--- dp_problem_for_two_arrays/dp_problem_for_two_arrays/test.cpp
+++ dp_problem_for_two_arrays/dp_problem_for_two_arrays/test.cpp
@@ -2,6 +2,22 @@
 //最长公共子序列（medium）
 //class Solution {
 //public:
+//    int maxUncrossedLines(vector<int>& nums1, vector<int>& nums2) {
+//        int size1 = nums1.size();
+//        int size2 = nums2.size();
+//        vector<vector<int>> dp(size1 + 1, vector<int>(size2 + 1));
+//        for (int i = 1; i <= size1; i++) {
+//            for (int j = 1; j <= size2; j++) {
+//                if (nums1[i - 1] == nums2[j - 1]) dp[i][j] = dp[i - 1][j - 1] + 1;
+//                else dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+//            }
+//        }
+//        return dp[size1][size2];
+//    }
+//};
+//最长公共子序列（medium）
+//class Solution {
+//public:
 //    int longestCommonSubsequence(string text1, string text2) {
 //        text1 = " " + text1;
 //        text2 = " " + text2;
