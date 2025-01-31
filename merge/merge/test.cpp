1db9166dbb535f68c3811ceb1c5abcc1ba4da6a4
zhou-liurong <19551230598@163.com>
2025-01-31 21:55:42 +0800

    作业

--- merge/merge/test.cpp
+++ merge/merge/test.cpp
@@ -72,42 +72,42 @@ using namespace std;
 //}
 
 //数组中的逆序对（hard）
-//class Solution {
-//	vector<int> tmp;
-//	int ret = 0;
-//public:
-//	int reversePairs(vector<int>& record) {
-//		tmp.resize(record.size());
-//		mergeSort(record, 0, record.size() - 1);
-//		return ret;
-//	}
-//	void mergeSort(vector<int>& nums, int left, int right)
-//	{
-//		if (left >= right) return;
-//		// 1. 选择中间点划分区间
-//		int mid = (left + right) >> 1;
-//		// [left, mid] [mid + 1, right]
-//		// 2. 把左右区间排序
-//		mergeSort(nums, left, mid);
-//		mergeSort(nums, mid + 1, right);
-//
-//		// 3. 合并两个有序数组
-//		int cur1 = left, cur2 = mid + 1, i = 0;
-//		while (cur1 <= mid && cur2 <= right) {
-//			if (nums[cur1] > nums[cur2]) {
-//				ret += right - cur2 + 1;
-//			}
-//			tmp[i++] = nums[cur1] > nums[cur2] ? nums[cur1++] : nums[cur2++];
-//		}
-//		// 处理没有遍历完的数组
-//		while (cur1 <= mid) tmp[i++] = nums[cur1++];
-//		while (cur2 <= right) tmp[i++] = nums[cur2++];
-//		// 4. 还原
-//		for (int i = left; i <= right; i++)
-//			nums[i] = tmp[i - left];
-//
-//	}
-//};
+class Solution {
+	vector<int> tmp;
+	int ret = 0;
+public:
+	int reversePairs(vector<int>& record) {
+		tmp.resize(record.size());
+		mergeSort(record, 0, record.size() - 1);
+		return ret;
+	}
+	void mergeSort(vector<int>& nums, int left, int right)
+	{
+		if (left >= right) return;
+		// 1. 选择中间点划分区间
+		int mid = (left + right) >> 1;
+		// [left, mid] [mid + 1, right]
+		// 2. 把左右区间排序
+		mergeSort(nums, left, mid);
+		mergeSort(nums, mid + 1, right);
+
+		// 3. 合并两个有序数组
+		int cur1 = left, cur2 = mid + 1, i = 0;
+		while (cur1 <= mid && cur2 <= right) {
+			if (nums[cur1] > nums[cur2]) {
+				ret += right - cur2 + 1;
+			}
+			tmp[i++] = nums[cur1] > nums[cur2] ? nums[cur1++] : nums[cur2++];
+		}
+		// 处理没有遍历完的数组
+		while (cur1 <= mid) tmp[i++] = nums[cur1++];
+		while (cur2 <= right) tmp[i++] = nums[cur2++];
+		// 4. 还原
+		for (int i = left; i <= right; i++)
+			nums[i] = tmp[i - left];
+
+	}
+};
 //
 //
 //int main() {
