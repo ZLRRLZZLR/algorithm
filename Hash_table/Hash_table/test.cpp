767c2eddcc33134b1ee09e82278c78e63d729ad5
zhou-liurong <19551230598@163.com>
2025-02-05 21:29:27 +0800

    判断是否互为字符重排（easy）

--- Hash_table/Hash_table/test.cpp
+++ Hash_table/Hash_table/test.cpp
@@ -1,4 +1,19 @@
 
+//判断是否互为字符重排（easy）
+//class Solution {
+//public:
+//    bool CheckPermutation(string s1, string s2) {
+//        if (s1.size() != s2.size()) return false;
+//        int arr[26] = { 0 };
+//        for (int i = 0; i < s1.size(); i++) {
+//            arr[s1[i] - 'a']++;
+//        }
+//        for (int i = 0; i < s1.size(); i++) {
+//            if (--arr[s2[i] - 'a'] < 0) return false;
+//        }
+//        return true;
+//    }
+//};
 //两数之和（easy）
 //class Solution {
 //public:
