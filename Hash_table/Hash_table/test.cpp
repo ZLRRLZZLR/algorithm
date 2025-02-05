84e077ca2ff0c797993fc7af877f38cc21030947
zhou-liurong <19551230598@163.com>
2025-02-05 22:23:07 +0800

    存在重复元素 II（easy）

--- Hash_table/Hash_table/test.cpp
+++ Hash_table/Hash_table/test.cpp
@@ -1,4 +1,17 @@
 
+//存在重复元素 II（easy）
+//class Solution {
+//public:
+//    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+//        unordered_map<int, int> hash;
+//        for (int i = 0; i < nums.size(); i++) {
+//            if (hash.count(nums[i]) && abs(hash[nums[i]] - i) <= k) return true;
+//            hash[nums[i]] = i;
+//        }
+//        return false;
+//    }
+//};
+
 //存在重复元素 I（easy）
 //class Solution {
 //public:
