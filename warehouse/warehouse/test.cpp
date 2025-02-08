3612e6214ff83a60f39095ae3cb1eae5595535fd
zhou-liurong <19551230598@163.com>
2025-02-08 22:35:14 +0800

    比较含退格的字符串（easy）

--- warehouse/warehouse/test.cpp
+++ warehouse/warehouse/test.cpp
@@ -5,6 +5,34 @@
 using namespace std;    
 
 
+//比较含退格的字符串（easy）
+//class Solution {
+//public:
+//    bool backspaceCompare(string s, string t) {
+//        string ret1;
+//        string ret2;
+//        for (auto ch : s) {
+//            if (!ret1.empty() && ch == '#') ret1.pop_back();
+//            else if (ch != '#') ret1 += ch;
+//        }
+//        for (auto ch : t) {
+//            if (!ret2.empty() && ch == '#') ret2.pop_back();
+//            else if (ch != '#') ret2 += ch;
+//        }
+//        return ret1 == ret2;
+//    }
+//};
+//class Solution {
+//public:
+//    string removeDuplicates(string s) {
+//        string ret;
+//        for (auto ch : s) {
+//            if (!ret.empty() && ret.back() == ch) ret.pop_back();
+//            else ret += ch;
+//        }
+//        return ret;
+//    }
+//};
 //删除字符中的所有相邻重复项（easy）
 //class Solution {
 //public:
@@ -20,9 +48,9 @@ using namespace std;
 //    }
 //};
 //
-//int main() {
-//	Solution sol;
-//	string s = "abbaca";
-//	sol.removeDuplicates(s);
-//	return 0;   
-//}
+int main() {
+	Solution sol;
+	string s = "abbaca";
+	sol.removeDuplicates(s);
+	return 0;   
+}
