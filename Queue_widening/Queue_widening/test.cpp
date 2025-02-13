c58d7e1f92fa57840b73c72438f62ba070690d08
zhou-liurong <19551230598@163.com>
2025-02-13 23:20:33 +0800

    二叉树的锯齿形层序遍历（medium）

--- Queue_widening/Queue_widening/test.cpp
+++ Queue_widening/Queue_widening/test.cpp
@@ -4,6 +4,45 @@
 
 using namespace std;
 
+///**¶þ²æÊ÷µÄ¾â³ÝÐÎ²ãÐò±éÀú£¨medium£©
+// * Definition for a binary tree node.
+// * struct TreeNode {
+// *     int val;
+// *     TreeNode *left;
+// *     TreeNode *right;
+// *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+// *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+// *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+// * };
+// */
+//class Solution {
+//public:
+//    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+//        vector<vector<int>> ret;
+//        if (root == nullptr) return ret;
+//        queue<TreeNode*> q;
+//        q.emplace(root);
+//        int levelsize = 1;
+//        int level = 0;
+//        while (q.size()) {
+//            vector<int> tmp;
+//            while (levelsize) {
+//                TreeNode* cur = q.front();
+//                q.pop();
+//                levelsize--;
+//                tmp.emplace_back(cur->val);
+//                if (cur->left) q.emplace(cur->left);
+//                if (cur->right) q.emplace(cur->right);
+//            }
+//            ret.emplace_back(tmp);
+//            levelsize = q.size();
+//            if (level % 2 != 0) reverse(ret[level].begin(), ret[level].end());
+//            level++;
+//        }
+//        return ret;
+//    }
+//};
+
 //N ²æÊ÷µÄ²ãÐò±éÀú£¨medium£©
 ///*
 //// Definition for a Node.
