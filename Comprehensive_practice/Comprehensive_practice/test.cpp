5eabb3f47d0f391f8994eee8ebdea87c9a41600c
zhou-liurong <19551230598@163.com>
2025-02-17 21:58:37 +0800

    被围绕的区域（medium）

--- Comprehensive_practice/Comprehensive_practice/test.cpp
+++ Comprehensive_practice/Comprehensive_practice/test.cpp
@@ -5,6 +5,128 @@
 
 using namespace std;
 
+
+//被围绕的区域（medium）
+//class Solution {
+//    int _m = 0;
+//    int _n = 0;
+//    int _ret = 0;
+//    int sum = 0;
+//public:
+//    void solve(vector<vector<char>>& board) {
+//        _m = board.size();
+//        _n = board[0].size();
+//
+//        for (int j = 0; j < _n; j++) {
+//            if (board[0][j] == 'O') {
+//                dfs(board, 0, j);
+//            }
+//        }
+//        for (int j = 0; j < _n; j++) {
+//            if (board[_m - 1][j] == 'O') {
+//                dfs(board, _m - 1, j);
+//            }
+//        }
+//        for (int i = 0; i < _m; i++) {
+//            if (board[i][0] == 'O') {
+//                dfs(board, i, 0);
+//            }
+//        }
+//        for (int i = 0; i < _m; i++) {
+//            if (board[i][_n - 1] == 'O') {
+//                dfs(board, i, _n - 1);
+//            }
+//        }
+//
+//        for (int i = 0; i < _m; i++) {
+//            for (int j = 0; j < _n; j++) {
+//                if (board[i][j] == 'O') {
+//                    board[i][j] = 'X';
+//                }
+//                else if (board[i][j] == '.') {
+//                    board[i][j] = 'O';
+//                }
+//            }
+//        }
+//    }
+//
+//    int dx[4] = { 0,0,1,-1 };
+//    int dy[4] = { 1,-1,0,0 };
+//
+//    void dfs(vector<vector<char>>& board, int i, int j) {
+//        board[i][j] = '.';
+//
+//        for (int k = 0; k < 4; k++) {
+//            int x = i + dx[k], y = j + dy[k];
+//            if (x >= 0 && x < _m && y >= 0 && y < _n && board[x][y] == 'O') {
+//                dfs(board, x, y);
+//            }
+//        }
+//    }
+//
+//};
+
+//int main() {
+//    vector<vector<char>> board = { {'O','O','O','O'},{'O','O','O','O'},{'O','O','O','O'},{'O','O','O','O'} };
+//	Solution().solve(board);
+//	return 0;
+//
+//}
+
+//class Solution {
+//    int _m = 0;
+//    int _n = 0;
+//    int _ret = 0;
+//    int sum = 0;
+//public:
+//    void solve(vector<vector<char>>& board) {
+//        _m = board.size();
+//        _n = board[0].size();
+//
+//        for (int i = 0; i < _m; i += _m - 1) {
+//            for (int j = 0; j < _n; j++) {
+//                if (board[0][j] == 'O') {
+//                    dfs(board, i, j);
+//                }
+//            }
+//        }
+//        for (int i = 0; i < _m; i++) {
+//            for (int j = 0; j < _n; j += _n - 1) {
+//                if (board[0][j] == 'O') {
+//                    dfs(board, i, j);
+//                }
+//            }
+//        }
+//        for (int i = 0; i < _m; i++) {
+//            for (int j = 0; j < _n; j++) {
+//                if (board[0][j] == 'O') {
+//                    board[0][j] = 'X';
+//                }
+//                if (board[0][j] == '.') {
+//                    board[0][j] = 'O';
+//                }
+//            }
+//        }
+//    }
+//
+//    int dx[4] = { 0,0,1,-1 };
+//    int dy[4] = { 1,-1,0,0 };
+//
+//    void dfs(vector<vector<char>>& board, int i, int j) {
+//        board[i][j] = '.';
+//
+//        for (int k = 0; k < 4; k++) {
+//            int x = i + dx[k], y = j + dy[k];
+//            if (x >= 0 && x < _m && y >= 0 && y < _n && board[x][y] == 'Q') {
+//                dfs(board, x, y);
+//            }
+//        }
+//    }
+//
+//};
+
+
+
 //岛屿的最大面积（medium）
 //class Solution {
 //    bool vis[301][301];
