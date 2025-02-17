97376a65af2837c32beb02057d109e2267e97dc0
zhou-liurong <19551230598@163.com>
2025-02-17 10:09:53 +0800

    单词搜索（medium）

--- Comprehensive_practice/Comprehensive_practice/test.cpp
+++ Comprehensive_practice/Comprehensive_practice/test.cpp
@@ -4,6 +4,93 @@
 using namespace std;
 
 
+//单词搜索（medium）
+//class Solution {
+//    bool vis[7][7];
+//    int _m = 0;
+//    int _n = 0;
+//public:
+//    bool exist(vector<vector<char>>& board, string word) {
+//        _m = board.size();
+//        _n = board[0].size();
+//        for (int i = 0; i < _m; i++) {
+//            for (int j = 0; j < _n; j++) {
+//                if (board[i][j] == word[0]) {
+//                    vis[i][j] = true;
+//                    if (dfs(board, i, j, word, 1)) return true;
+//                    vis[i][j] = false;
+//                }
+//            }
+//        }
+//        return false;
+//    }
+//    int dx[4] = { 0,0,1,-1 };
+//    int dy[4] = { 1,-1,0,0 };
+//
+//    bool dfs(vector<vector<char>>& board, int i, int j, string word, int pos) {
+//        if (pos == word.size()) return true;
+//
+//        for (int k = 0; k < 4; k++) {
+//            int x = i + dx[k], y = j + dy[k];
+//            if (x >= 0 && x < _m && y >= 0 && y < _n && !vis[x][y] && board[x][y] == word[pos]) {
+//                vis[x][y] = true;
+//                if (dfs(board, x, y, word, pos + 1)) return true;
+//                vis[x][y] = false;
+//            }
+//        }
+//        return false;
+//    }
+//};
+
+/*int main() {
+	vector<vector<char>> board = { {'A','B','C','E'},{'S','F','C','S'},{'A','D','E','E'} };
+	string word = "ABCCED";
+	Solution().exist(board, word);
+	return 0;
+} */  
+
+
+
+
+//class Solution {
+//    bool row[9][10];
+//    bool col[9][10];
+//    bool grid[3][3][10];
+//    int _i = 0;
+//    int _j = 0;
+//public:
+//    void solveSudoku(vector<vector<char>>& board) {
+//        for (int i = 0; i < 9; i++) {
+//            for (int j = 0; j < 9; j++) {
+//                if (isdigit(board[i][j])) {
+//                    int num = board[i][j] - '0';
+//                    row[i][num] = col[j][num] = grid[i / 3][j / 3][num] = true;
+//                }
+//            }
+//        }
+//        dfs(board);
+//    }
+//    bool dfs(vector<vector<char>>& board) {
+//        for (int i = 0; i < 9; i++) {
+//            for (int j = 0; j < 9; j++) {
+//                if (!isdigit(board[i][j])) {
+//                    for (int num = 1; num <= 9; num++) {
+//                        if (!row[i][num] && !col[j][num] && !grid[i / 3][j / 3][num]) {
+//                            board[i][j] = num + '0';
+//                            if (dfs(board) == true) return true;
+//                            board[i][j] = '.';
+//                            row[i][num] = col[j][num] = grid[i / 3][j / 3][num] = false;
+//                        }
+//                    }
+//                    return false;
+//                }
+//            }
+//        }
+//        return true;
+//    }
+//};
+
+
 //有效的数独（medium）
 //class Solution {
 //public:
