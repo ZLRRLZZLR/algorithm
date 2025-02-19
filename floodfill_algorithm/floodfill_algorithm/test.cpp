5f44633ff2a0dc44cf2b75782149fcdafdf76b26
zhou-liurong <19551230598@163.com>
2025-02-19 16:08:55 +0800

    扫雷游戏（medium）

--- floodfill_algorithm/floodfill_algorithm/test.cpp
+++ floodfill_algorithm/floodfill_algorithm/test.cpp
@@ -6,6 +6,61 @@
 
 using namespace std;
 
+//扫雷游戏（medium）
+//class Solution {
+//    int _m = 0;
+//    int _n = 0;
+//
+//    int dx[8] = { 0 , 0 , 1 , -1 , -1 , -1 , 1 , 1 };
+//    int dy[8] = { 1 , -1 , 0 , 0 , -1 , 1 , -1 , 1 };
+//
+//public:
+//    vector<vector<char>> updateBoard(vector<vector<char>>& board, vector<int>& click) {
+//        _m = board.size();
+//        _n = board[0].size();
+//        int x = click[0], y = click[1];
+//        if (board[x][y] == 'M') {
+//            board[x][y] = 'X';
+//            return board;
+//        }
+//        dfs(board, x, y);
+//        return board;
+//    }
+//
+//    void dfs(vector<vector<char>>& board, int i, int j) {
+//
+//        int count = 0;
+//        for (int k = 0; k < 8; k++) {
+//            int x = i + dx[k], y = j + dy[k];
+//            if (x >= 0 && x < _m && y >= 0 && y < _n && board[x][y] == 'M') {
+//                count++;
+//            }
+//            else {
+//            }
+//        }
+//        if (count > 0) {
+//            board[i][j] = count + '0';
+//            return;
+//        }
+//        else {
+//            board[i][j] = 'B';
+//            for (int k = 0; k < 8; k++) {
+//                int x = i + dx[k], y = j + dy[k];
+//                if (x >= 0 && x < _m && y >= 0 && y < _n && board[x][y] == 'E') {
+//                    dfs(board, x, y);
+//                }
+//            }
+//        }
+//
+//    }
+//};
+//int main() {
+//	vector<vector<char>> board = { {'E','E','E','E','E'},{'E','E','M','E','E'},{'E','E','E','E','E'},{'E','E','E','E','E'} };
+//	vector<int> click = { 3,0 };
+//	Solution().updateBoard(board, click);
+//	return 0;
+//}
+
 //太平洋大西洋水流问题（medium）
 //class Solution {
 //    int _m = 0;
