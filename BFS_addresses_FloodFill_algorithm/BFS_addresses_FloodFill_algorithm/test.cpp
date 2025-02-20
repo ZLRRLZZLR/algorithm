33267df318f0210c42b351551f7f51c4a96674c5
zhou-liurong <19551230598@163.com>
2025-02-20 23:52:12 +0800

    岛屿数量（medium）

--- BFS_addresses_FloodFill_algorithm/BFS_addresses_FloodFill_algorithm/test.cpp
+++ BFS_addresses_FloodFill_algorithm/BFS_addresses_FloodFill_algorithm/test.cpp
@@ -8,6 +8,40 @@
 #include <set>
 using namespace std;
 
+//µ∫”Ï ˝¡ø£®medium£©
+//class Solution {
+//    typedef pair<int, int> PII;
+//    int dx[4] = { 0,0,1,-1 };
+//    int dy[4] = { 1,-1,0,0 };
+//public:
+//    int numIslands(vector<vector<char>>& grid) {
+//        int m = grid.size(), n = grid[0].size();
+//        int ret = 0;
+//        for (int i = 0; i < m; i++) {
+//            for (int j = 0; j < n; j++) {
+//                if (grid[i][j] == '1') {
+//                    ret++;
+//                    queue<PII> q;
+//                    q.emplace(i, j);
+//                    while (q.size()) {
+//                        auto [a, b] = q.front();
+//                        grid[a][b] = '0';
+//                        q.pop();
+//                        for (int k = 0; k < 4; k++) {
+//                            int x = a + dx[k], y = b + dy[k];
+//                            if (x >= 0 && x < m && y >= 0 && y < n && grid[x][y] == '1') {
+//                                q.emplace(x, y);
+//                                grid[x][y] = '0';
+//                            }
+//                        }
+//                    }
+//                }
+//            }
+//        }
+//        return ret;
+//    }
+//};
+
 //ÕºœÒ‰÷»æ£®medium£©
 //class Solution {
 //    typedef pair<int, int> PII;
