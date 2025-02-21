2d83b6f505c2cb1818ec8c9b01660c6521a837f4
zhou-liurong <19551230598@163.com>
2025-02-21 10:18:25 +0800

    被围绕的区域（medium）

--- BFS_addresses_FloodFill_algorithm/BFS_addresses_FloodFill_algorithm/test.cpp
+++ BFS_addresses_FloodFill_algorithm/BFS_addresses_FloodFill_algorithm/test.cpp
@@ -8,6 +8,172 @@
 #include <set>
 using namespace std;
 
+
+//被围绕的区域（medium）
+//class Solution {
+//    typedef pair<int, int> PII;
+//    bool vis[201][201];
+//    int _m = 0;
+//    int _n = 0;
+//    int _ret = 0;
+//    int dx[4] = { 0,0,1,-1 };
+//    int dy[4] = { 1,-1,0,0 };
+//public:
+//    void solve(vector<vector<char>>& board) {
+//        _m = board.size();
+//        _n = board[0].size();
+//        if (_m == 1 && _n == 1) return;
+//        for (int j = 0; j < _n; j++) {
+//            if (board[0][j] == 'O' && !vis[0][j]) {
+//                bfs(board, 0, j);
+//            }
+//            if (board[_m - 1][j] == 'O' && !vis[_m - 1][j]) {
+//                bfs(board, _m - 1, j);
+//            }
+//        }
+//        for (int i = 0; i < _m; i++) {
+//            if (board[i][0] == 'O' && !vis[i][0]) {
+//                bfs(board, i, 0);
+//            }
+//            if (board[i][_n - 1] == 'O' && !vis[i][_n - 1]) {
+//                bfs(board, i, _n - 1);
+//            }
+//        }
+//        for (int i = 0; i < _m; i++) {
+//            for (int j = 0; j < _n; j++) {
+//                if (board[i][j] == 'O' && !vis[i][j]) board[i][j] = 'X';
+//                if (board[i][j] == '.') board[i][j] = 'O';
+//            }
+//        }
+//    }
+//    void bfs(vector<vector<char>>& board, int i, int j) {
+//        int sum = 0;
+//        queue<PII> q;
+//        q.emplace(i, j);
+//        vis[i][j] = true;
+//        board[i][j] = '.';
+//        while (q.size()) {
+//            auto [a, b] = q.front();
+//            q.pop();
+//            for (int k = 0; k < 4; k++) {
+//                int x = a + dx[k], y = b + dy[k];
+//                if (x >= 0 && x < _m && y >= 0 && y < _n && !vis[x][y] && board[x][y] == 'O') {
+//                    q.emplace(x, y);
+//                    vis[x][y] = true;
+//                    board[i][j] = '.';
+//                }
+//            }
+//        }
+//    }
+//};
+//class Solution {
+//    typedef pair<int, int> PII;
+//    bool vis[201][201];
+//    int _m = 0;
+//    int _n = 0;
+//    int _ret = 0;
+//    int dx[4] = { 0,0,1,-1 };
+//    int dy[4] = { 1,-1,0,0 };
+//public:
+//    void solve(vector<vector<char>>& board) {
+//        _m = board.size();
+//        _n = board[0].size();
+//
+//        for (int j = 0; j < _n; j++) {
+//            if (board[0][j] == 'O' && !vis[0][j]) {
+//                bfs(board, 0, j);
+//            }
+//            if (board[_m - 1][j] == 'O' && !vis[_m - 1][j]) {
+//                bfs(board, _m - 1, j);
+//            }
+//        }
+//        for (int i = 0; i < _m; i++) {
+//            if (board[i][0] == 'O' && !vis[i][0]) {
+//                bfs(board, i, 0);
+//            }
+//            if (board[i][_n - 1] == 'O' && !vis[i][_n - 1]) {
+//                bfs(board, i, _n - 1);
+//            }
+//        }
+//        for (int i = 1; i < _m; i++) {
+//            for (int j = 0; j < _n; j++) {
+//                if (board[i][j] == 'O' && !vis[i][j]) board[i][j] = 'X';
+//                if (board[i][j] == '.') board[i][j] = 'O';
+//            }
+//        }
+//    }
+//    int bfs(vector<vector<char>>& board, int i, int j) {
+//        int sum = 0;
+//        queue<PII> q;
+//        q.emplace(i, j);
+//        vis[i][j] = true;
+//        board[i][j] = '.';
+//            while (q.size()) {
+//                auto [a, b] = q.front();
+//                q.pop();
+//                for (int k = 0; k < 4; k++) {
+//                    int x = a + dx[k], y = b + dy[k];
+//                    if (x >= 0 && x < _m && y >= 0 && y < _n && !vis[x][y] && board[x][y] == 'O') {
+//                        q.emplace(x, y);
+//                        vis[x][y] = true;
+//                        board[i][j] = '.';
+//                    }
+//                }
+//            }
+//    }
+//};
+
+
+
+
+//岛屿的最大面积（medium）
+//class Solution {
+//    typedef pair<int, int> PII;
+//    bool vis[301][301];
+//    int _m = 0;
+//    int _n = 0;
+//    int _ret = 0;
+//    int dx[4] = { 0,0,1,-1 };
+//    int dy[4] = { 1,-1,0,0 };
+//
+//public:
+//    int maxAreaOfIsland(vector<vector<int>>& grid) {
+//        _m = grid.size();
+//        _n = grid[0].size();
+//        for (int i = 0; i < _m; i++) {
+//            for (int j = 0; j < _n; j++) {
+//                if (grid[i][j] == 1 && !vis[i][j]) {
+//                    int sum = 0;
+//                    queue<PII> q;
+//                    q.emplace(i, j);
+//                    vis[i][j] = true;
+//                    sum++;
+//                    while (q.size()) {
+//                        auto [a, b] = q.front();
+//                        q.pop();
+//                        for (int k = 0; k < 4; k++) {
+//                            int x = a + dx[k], y = b + dy[k];
+//                            if (x >= 0 && x < _m && y >= 0 && y < _n && !vis[x][y] && grid[x][y] == 1) {
+//                                q.emplace(x, y);
+//                                vis[x][y] = true;
+//                                sum++;
+//                            }
+//                        }
+//                    }
+//                    _ret = max(sum, _ret);
+//                }
+//            }
+//        }
+//        return _ret;
+//    }
+//};
+//
+//int main() {
+//	vector<vector<int>> grid = { {1,1,0,0,0},{1,1,0,0,0},{0,0,0,1,1},{0,0,0,1,1} };
+//	Solution().maxAreaOfIsland(grid);
+//    return 0;
+//}
+
 //岛屿数量（medium）
 //class Solution {
 //    typedef pair<int, int> PII;
