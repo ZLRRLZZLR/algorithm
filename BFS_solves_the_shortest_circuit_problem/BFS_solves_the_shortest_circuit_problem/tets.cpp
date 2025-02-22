bea346198cf15a2a281a39d6425900a617370b3e
zhou-liurong <19551230598@163.com>
2025-02-22 15:55:10 +0800

    迷宫中离入口最近的出口（medium）

---
+++ BFS_solves_the_shortest_circuit_problem/BFS_solves_the_shortest_circuit_problem/tets.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<map>
+#include<set>
+#include<unordered_map>
+#include<unordered_set>
+#include<algorithm>
+
+using namespace std;
+
+
+
+//迷宫中离入口最近的出口（medium）
+//class Solution {
+//    typedef pair<int, int> PII;
+//    bool vis[201][201];
+//    int _m = 0;
+//    int _n = 0;
+//    int _ret = 0;
+//    int dx[4] = { 0,0,1,-1 };
+//    int dy[4] = { 1,-1,0,0 };
+//public:
+//    int nearestExit(vector<vector<char>>& maze, vector<int>& entrance) {
+//        _m = maze.size();
+//        _n = maze[0].size();
+//        return bfs(maze, entrance[0], entrance[1]);
+//    }
+//
+//    int bfs(vector<vector<char>>& maze, int i, int j) {
+//        int levelsize = 0;
+//        queue<PII> q;
+//        q.emplace(i, j);
+//        int level = 1;
+//        vis[i][j] = true;
+//        while (q.size()) {
+//            while (level--) {
+//                int a = q.front().first;
+//                int b = q.front().second;
+//                q.pop();
+//                if ((a == 0 || b == 0 || a == _m - 1 || b == _n - 1) && (a != i || b != j)) {
+//                    return levelsize;
+//                }
+//                for (int k = 0; k < 4; k++) {
+//                    int x = a + dx[k], y = b + dy[k];
+//                    if (x >= 0 && x < _m && y >= 0 && y < _n && !vis[x][y] && maze[x][y] == '.') {
+//                        q.emplace(x, y);
+//                        vis[x][y] = true;
+//                    }
+//                }
+//            }
+//            level = q.size();
+//            levelsize++;
+//        }
+//        return -1;
+//    }
+//};
+//
+//int main() {
+//	Solution s;
+//	vector<vector<char>> maze = { {'+','+','.','+'},{'.','.','.','+'},{'+','+','+','.'} };
+//	vector<int> entrance = { 1,2 };
+//	cout << s.nearestExit(maze, entrance) << endl;
+//	return 0;
+//}
