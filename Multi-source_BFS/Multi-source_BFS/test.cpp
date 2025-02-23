0f0cf3dbae161574380f075cdeeb7c6d0809c26c
zhou-liurong <19551230598@163.com>
2025-02-23 13:54:19 +0800

    飞地的数量（medium）

--- Multi-source_BFS/Multi-source_BFS/test.cpp
+++ Multi-source_BFS/Multi-source_BFS/test.cpp
@@ -11,6 +11,58 @@
 
 using namespace std;
 
+//飞地的数量（medium）
+//class Solution {
+//    typedef pair<int, int> PII;
+//    bool vis[501][501];
+//    int dx[4] = { 0,0,1,-1 };
+//    int dy[4] = { 1,-1,0,0 };
+//public:
+//    int numEnclaves(vector<vector<int>>& grid) {
+//        int m = grid.size();
+//        int n = grid[0].size();
+//        queue<PII> q;
+//        for (int i = 0; i < m; i++) {
+//            if (grid[i][0] == 1) {
+//                q.emplace(i, 0);
+//                vis[i][0] = true;
+//            }
+//            if (grid[i][n - 1] == 1) {
+//                q.emplace(i, n - 1);
+//                vis[i][n - 1] = true;
+//            }
+//        }
+//        for (int j = 0; j < n; j++) {
+//            if (grid[0][j] == 1) {
+//                q.emplace(0, j);
+//                vis[0][j] = true;
+//            }
+//            if (grid[m - 1][j] == 1) {
+//                q.emplace(m - 1, j);
+//                vis[m - 1][j] = true;
+//            }
+//        }
+//        while (q.size()) {
+//            auto [a, b] = q.front();
+//            q.pop();
+//            for (int k = 0; k < 4; k++) {
+//                int x = a + dx[k], y = b + dy[k];
+//                if (x >= 0 && x < m && y >= 0 && y < n && !vis[x][y] && grid[x][y] == 1) {
+//                    q.emplace(x, y);
+//                    vis[x][y] = true;
+//                }
+//            }
+//        }
+//        int ret = 0;
+//        for (int i = 0; i < m; i++) {
+//            for (int j = 0; j < n; j++) {
+//                if (!vis[i][j] && grid[i][j] == 1) ret++;
+//            }
+//        }
+//        return ret;
+//    }
+//};
+
 //01 矩阵（medium）
 //class Solution {
 //    typedef pair<int, int> PII;
