5e3030deb9b6ba51b7ff0544ba1a80a2db6c071c
zhou-liurong <19551230598@163.com>
2025-02-23 22:42:32 +0800

    课程表II（medium）

--- BFS_solves_topological_sorting/BFS_solves_topological_sorting/test.cpp
+++ BFS_solves_topological_sorting/BFS_solves_topological_sorting/test.cpp
@@ -4,6 +4,43 @@
 #include<unordered_map>
 using namespace std;
 
+//课程表II（medium）
+//class Solution {
+//public:
+//    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+//        unordered_map<int, vector<int>> edges;
+//        vector<int> in(numCourses);
+//        vector<int> ret;
+//        for (auto& i : prerequisites) {
+//            int a = i[0], b = i[1];
+//            edges[b].push_back(a);
+//            in[a]++;
+//        }
+//
+//        queue<int> q;
+//        for (int i = 0; i < numCourses; i++) {
+//            if (in[i] == 0) q.emplace(i);
+//        }
+//
+//        while (q.size()) {
+//            auto  front = q.front();
+//            ret.emplace_back(front);
+//            q.pop();
+//
+//            for (auto j : edges[front]) {
+//                in[j]--;
+//                if (in[j] == 0) q.emplace(j);
+//            }
+//        }
+//
+//        for (auto i : in)
+//            if (i)
+//                return vector<int>();
+//
+//        return ret;
+//    }
+//};
+
 //课程表（medium）
 //class Solution {
 //public:
