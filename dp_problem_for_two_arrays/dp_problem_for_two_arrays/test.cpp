def59f8dfd61e9681778a3c2bab6edfd47bc73b7
zhou-liurong <19551230598@163.com>
2025-02-27 19:24:16 +0800

    两个字符串的最小 ASCII 删除和（medium）

--- dp_problem_for_two_arrays/dp_problem_for_two_arrays/test.cpp
+++ dp_problem_for_two_arrays/dp_problem_for_two_arrays/test.cpp
@@ -5,6 +5,26 @@
 using namespace std;
 
 
+//两个字符串的最小 ASCII 删除和（medium）
+//class Solution {
+//public:
+//    int minimumDeleteSum(string s1, string s2) {
+//        int m = s1.size();
+//        int n = s2.size();
+//        vector<vector<int>> dp(m + 1, vector<int>(n + 1));
+//        for (int i = 1; i <= m; i++) {
+//            for (int j = 1; j <= n; j++) {
+//                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+//                if (s1[i - 1] == s2[j - 1]) dp[i][j] = max(dp[i - 1][j - 1] + s1[i - 1], dp[i][j]);
+//            }
+//        }
+//        int sum = 0;
+//        for (auto ch : s1) sum += ch;
+//        for (auto ch : s2) sum += ch;
+//
+//        return sum - dp[m][n] * 2;
+//    }
+//};
 //交错字符串（medium）
 //class Solution {
 //public:
