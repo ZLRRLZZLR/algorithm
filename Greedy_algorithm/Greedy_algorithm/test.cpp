9021013ba7296318fee18fd2cc20f326a17b5604
zhou-liurong <19551230598@163.com>
2025-03-12 23:14:49 +0800

    将数组和减半的最少操作次数（medium）

--- Greedy_algorithm/Greedy_algorithm/test.cpp
+++ Greedy_algorithm/Greedy_algorithm/test.cpp
@@ -3,6 +3,30 @@
 #include<algorithm>
 using namespace std;
 
+//将数组和减半的最少操作次数（medium）
+//class Solution {
+//public:
+//    int halveArray(vector<int>& nums) {
+//        priority_queue<double> heap;
+//        double sum = 0;
+//        for (auto i : nums) {
+//            sum += i;
+//            heap.emplace(i);
+//        }
+//
+//        int ret = 0;
+//        double aim = sum / 2;
+//        while (sum > aim) {
+//            ret++;
+//            double top = heap.top() / 2;
+//            sum -= top;
+//            heap.pop();
+//            heap.emplace(top);
+//        }
+//
+//        return ret;
+//    }
+//};
 //柠檬水找零（easy）
 //class Solution {
 //public:
