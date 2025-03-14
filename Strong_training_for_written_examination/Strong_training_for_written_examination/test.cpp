ded2aad93e08b74bc86946239aa90c15f3fdc3e8
zhou-liurong <19551230598@163.com>
2025-03-14 23:57:00 +0800

    孩子们的游戏（约瑟夫环）

--- Strong_training_for_written_examination/Strong_training_for_written_examination/test.cpp
+++ Strong_training_for_written_examination/Strong_training_for_written_examination/test.cpp
@@ -5,6 +5,27 @@
 using namespace std;
 
 //笔试强训day-5
+// 孩子们的游戏（约瑟夫环）
+// 动态规划
+//class Solution {
+//public:
+//    /**
+//     * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+//     *
+//     *
+//     * @param n int整型
+//     * @param m int整型
+//     * @return int整型
+//     */
+//    int LastRemaining_Solution(int n, int m) {
+//        int f = 0;
+//        for (int i = 1; i <= n; i++) {
+//            f = (f + m) % i;
+//        }
+//        return f;
+//    }
+//};
+
 //#include<iostream>
 //#include<math.h>
 //
