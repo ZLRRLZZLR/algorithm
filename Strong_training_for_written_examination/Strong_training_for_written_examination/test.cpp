5021e789e639734da601307f7364a738df32c519
zhou-liurong <19551230598@163.com>
2025-03-23 23:45:35 +0800

    拼三角

--- Strong_training_for_written_examination/Strong_training_for_written_examination/test.cpp
+++ Strong_training_for_written_examination/Strong_training_for_written_examination/test.cpp
@@ -4,6 +4,31 @@
 
 using namespace std;
 
+//拼三角
+//#include<iostream>
+//#include<algorithm>
+//
+//using namespace std;
+//int t;
+//int arr[6];
+//
+//int main() {
+//    cin >> t;
+//    while (t--)
+//    {
+//        for (int i = 0; i < 6; i++) cin >> arr[i];
+//        sort(arr, arr + 6);
+//        if (arr[0] + arr[1] > arr[2] && arr[3] + arr[4] > arr[5] ||
+//            arr[0] + arr[2] > arr[3] && arr[1] + arr[4] > arr[5] ||
+//            arr[0] + arr[3] > arr[4] && arr[1] + arr[2] > arr[5] ||
+//            arr[0] + arr[4] > arr[5] && arr[1] + arr[2] > arr[3])
+//        {
+//            cout << "Yes" << endl;
+//        }
+//        else cout << "No" << endl;
+//    }
+//    return 0;
+//}
 //笔试强训day-6
 // 大数乘法
 //class Solution {
